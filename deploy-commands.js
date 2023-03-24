936a1b5d467b10cf7cd56bbd6ed2856b0ceb39a3
ChirayuGupta2003 <chirayugupta2003@gmail.com>
2023-03-24 17:05:17 +0530

    changed deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -26,7 +26,7 @@ const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
         const data = await rest.put(
             Routes.applicationGuildCommands(
                 process.env.CLIENT_ID,
-                process.env.GUILD_ID
+                // process.env.GUILD_ID
             ),
             { body: commands }
         );
